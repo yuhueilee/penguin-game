6c7a27722d15c40936491b8dc83073d8fa9158bf
yuhueilee <ylee0026@student.monash.edu>
2024-01-23 11:41:14 +0800

    Add turn propoerty to Game state

--- src/Game.js
+++ src/Game.js
@@ -1,11 +1,17 @@
 import { RandomIntArray } from "./shared/Helpers";
 import { INVALID_MOVE } from 'boardgame.io/core';
 
+// PenguinFive defines the game state.
 export const PenguinFive = {
   setup: () => ({
     cells: Array(9).fill(null),
     cellsPoints: RandomIntArray(1, 3, 9)
   }),
+
+  turn: {
+    minMoves: 1,
+    maxMoves: 1, // tell the framework to automatically end a player’s turn after a single move has been made
+  },
 
   moves: {
     clickCell: ({ G, playerID }, id) => {
