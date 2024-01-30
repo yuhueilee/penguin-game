0d35d56906aad64e199571c4c615980bf3322c8a
yuhueilee <wendylee70127@gmail.com>
2024-01-30 11:00:56 +0800

    Add constants

--- src/shared/Consts.ts
+++ src/shared/Consts.ts
@@ -3,3 +3,7 @@ export const totalCells = 8;
 export const maxCellsPerRow = 3;
 
 export const maxLaboursPerPlayer = 2;
+
+export const iRange = [-1, -1, +0, +1, +1, +0];
+
+export const jRange = [-1, +1, +1, +1, -1, -1];
