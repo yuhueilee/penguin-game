62d86be95b3d91832cab07a0424e2bed30a7d0f7
yuhueilee <wendylee70127@gmail.com>
2024-02-09 11:19:23 +0800

    Adjust the position for leaderboard and player turn

--- src/Board.tsx
+++ src/Board.tsx
@@ -106,15 +106,17 @@ export function PenguinBattleBoard({
 
     return (
         <div className="board">
-            <div className="playerTurn">
-                <h1 className="title">Now Playing...</h1>
-                {PenguinIcon(currPlayerID)}
+            <div className="column-1">
+                <div className="playerTurn">
+                    <h1 className="title">Now Playing...</h1>
+                    {PenguinIcon(currPlayerID)}
+                </div>
+                <div className="leaderboard">
+                    <h1 className="title">LeaderBoard</h1>
+                    {ranking}
+                </div>
             </div>
             <div className="table">{tbody}</div>
-            <div className="leaderboard">
-                <h1 className="title">LeaderBoard</h1>
-                {ranking}
-            </div>
         </div>
     );
 }
